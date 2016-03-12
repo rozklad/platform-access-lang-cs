e0fa06d5b2b4d25a734ee24a631724bb8fea8fab
rozklad <jan.rozklad@gmail.com>
2016-03-12 16:16:24 +0100

    Initial commit

---
+++ common.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of the Platform Access extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the WTFPL license.
+ *
+ * @package    Platform Access extension
+ * @version    3.0.0
+ * @author     Sanatorium
+ * @license    WTFPL
+ * @copyright  2016, Sanatorium
+ * @link       http://sanatorium.ninja
+ */
+
+return [
+
+	'title' => 'Oprávnění',
+
+];
---
+++ permissions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of the Platform Access extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the WTFPL license.
+ *
+ * @package    Platform Access extension
+ * @version    3.0.0
+ * @author     Sanatorium
+ * @license    WTFPL
+ * @copyright  2016, Sanatorium
+ * @link       http://sanatorium.ninja
+ */
+
+return [
+
+	'no_access'    => "Nemáte přístup do této oblasti.",
+	'no_access_to' => "Nemáte přístup k [:permission].",
+
+];
